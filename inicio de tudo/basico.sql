4f9a988a419930ea61a641b630c29a7286b86756
thiago de lima <thiagolimasilva689@gmail.com>
2026-05-06 14:48:16 -0400

    Update basico.sql

--- inicio de tudo/basico.sql
+++ inicio de tudo/basico.sql
@@ -4,5 +4,7 @@ where actor_id = 1
 order by last_name
 ;
 #where consulta os dados das linhas, indo atrás deles
+#select consulta dados do banco de dados
+#use você selecionar um banco de dados específico 
 
 SELECT *from movie where  title between 1 and 4
