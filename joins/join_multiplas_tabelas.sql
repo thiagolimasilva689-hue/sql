6797a9ad2d930e71da1c930cae6535cbfff07cdf
thiago de lima <thiagolimasilva689@gmail.com>
2026-05-09 14:49:06 -0400

    Update join_multiplas_tabelas.sql

--- joins/join_multiplas_tabelas.sql
+++ joins/join_multiplas_tabelas.sql
@@ -15,3 +15,21 @@ select
 from customer
 join rental
           on customer.customer_id = rental.customer_id
+
+
+exemplo3:
+select
+    customer.first_name,
+     city.city,
+     rental.rental_date
+from customer
+join address
+         on customer.address_id =  address.address_id
+join rental
+         on customer.customer_id = rental.customer_id
+join city
+		on address.city_id = city.city_id
+where city.city= 'London'     
+ order by rental.rental_date desc;
+
+
